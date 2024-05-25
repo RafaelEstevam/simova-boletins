0c7d5f3920e3e7e9ef16adba9e58232e6317702a
Rafael Estevam <rafael.estevamdesign@gmail.com>
2024-05-25 16:11:59 -0300

    First commit

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import LoginView from '../views/Login/LoginView.vue'
+import EmployeesView from '../views/Employees/EmployeesView.vue'
+import BulletinsView from '../views/Bulletins/BulletinsView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: LoginView
+    },
+    {
+      path: '/employees',
+      name: 'employees',
+      component: EmployeesView
+    },
+    {
+      path: '/bulletins',
+      name: 'bulletins',
+      component: BulletinsView
+    },
+  ]
+})
+
+export default router
