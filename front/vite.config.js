384be0389539a167d272cfbf36d1ee1da78df6b5
rafaelEstevam <rafael.estevamdesign@gmail.com>
2024-05-29 16:53:49 -0300

    Adding redirects

--- front/vite.config.js
+++ front/vite.config.js
@@ -3,12 +3,21 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: '_redirects',
+          dest: ''
+        }
+      ]
+    })
   ],
   resolve: {
     alias: {
